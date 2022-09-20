e1baf4443ae9eea1dc2d22321aa4dd080824bb30
eatmjtweets <111845489+eatmjtweets@users.noreply.github.com>
2022-09-19 21:43:37 -0600

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,14 @@
+      //ONCLICK BUTTON
+      function My_First_Function () {
+        var str = "This text is Violet!";
+        var result = str.fontcolor("violet");
+        document.getElementById("Violet_Text").innerHTML = result;   
+  }
+  //END OF ONCLICK BUTTON
+
+
+      function myFunction () {
+        var sentence = "I am learning";
+        sentence += " a lot from this book!";
+        document.getElementById("Concatenate").innerHTML = sentence;
+      }
