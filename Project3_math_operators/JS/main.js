5c59985382174fd78207f29095f489ba7a333db9
eatmjtweets <111845489+eatmjtweets@users.noreply.github.com>
2022-11-29 15:41:17 -0700

    Add files via upload

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,37 +1,38 @@
 function addition_Function() {
-  var addition = 2 + 2;
-  document.getElementbyID("Math").innerHTML = "2 + 2 = " + addition;
+  var number =  2 + 2;
+  document.getElementById("Math1").innerHTML = "Sum of 2 + 2 is: " + number;
 }
 
 
 function subtraction_Function() {
-  var subtraction = 5 - 2;
-  document.getElementById("Math").innerHTML = "5 - 2 = " + subtraction;
+  var number = 5 - 2;
+  document.getElementById("Math2").innerHTML = "Difference of 5 - 2 is: " + number;
 }
 
+
 function multiplication() {
-  var simple_Math = 6 * 8;
-  document.getElementById("Math").innerHTML = "6 x 8 = " + simple_Math;
+  var number = 6 * 8;
+  document.getElementById("Math3").innerHTML = "6 x 8 = " + number;
 }
 
 function division() {
-  var simple_Math = 48/6;
-  document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+  var number = 48/6;
+  document.getElementById("Math4").innerHTML = "48 / 6 = " + number;
 }
 
 function more_Math() {
-  var simple_Math = (1 + 2) * 10 / 2 -5;
-  document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+  var number = (1 + 2) * 10 / 2 -5;
+  document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + number;
 }
 
 function modulus_Operator() {
-  var simple_Math = 25 % 6;
-  document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+  var number = 25 % 6;
+  document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a remainder of: " + number;
 }
 
 function negation_Operator() {
-  var x = 10;
-  document.getElementById("Math").innerHTML = -x;
+  var number = 10;
+  document.getElementById("Math7").innerHTML = -number;
 }
 
 
@@ -40,7 +41,7 @@ function negation_Operator() {
 
 var X = 5;
 X++;
-document.write(X);
+document.write(X + " ");
 
 
 
